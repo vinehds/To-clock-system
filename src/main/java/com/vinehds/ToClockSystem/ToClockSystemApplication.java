3fdbb3b1f96b8624fc40c94c3e3a70426dbca195
Vinicius Dalaqua <viniciushdalaqua@gmail.com>
2024-06-14 19:46:47 -0300

    Project Created

---
+++ src/main/java/com/vinehds/ToClockSystem/ToClockSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vinehds.ToClockSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToClockSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToClockSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinehds/ToClockSystem/ToClockSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinehds.ToClockSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToClockSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
